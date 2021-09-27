1924f7ee00bc30072db80d1bf9b03de96168da02
eklebaba8 <eklebaba8@gmail.com>
2021-09-27 17:54:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Review from './Review';
+import {FaBeer} from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+       <h2> Reviews projesi <FaBeer /> </h2> 
+
+       <section className='container'>
+         <div className='title'>
+           <h2>our reviews</h2>
+           <div className='underline'></div>
+            </div>
+           <Review/>
+       </section>
+    </main>
+ 
   );
 }
 
